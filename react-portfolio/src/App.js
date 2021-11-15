40514424e6de78069597e9c6613e763ea03338a1
unknown <nayefkanaan03@gmail.com>
2021-11-15 03:57:07 +0300

    Adding toggle use-state func

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Home from '../src/pages/index'
 
 
 function App() {
   return (
    <div>
-     <Navbar />
+     <Home />
    </div>
   );
 }
