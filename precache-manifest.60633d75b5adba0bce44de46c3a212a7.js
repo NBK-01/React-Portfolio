433c04b2d87e11d6c45d84ccd988c1c1f2cae903
unknown <nayefkanaan03@gmail.com>
2021-11-19 18:11:59 +0300

    Updates

---
+++ precache-manifest.60633d75b5adba0bce44de46c3a212a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c505044b677c160fceec0841b5a12c1",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "4947a760b936f36dff7f",
+    "url": "/React-Portfolio/static/css/main.869f008c.chunk.css"
+  },
+  {
+    "revision": "b2a2a2ed2403fd7cb5ec",
+    "url": "/React-Portfolio/static/js/2.d66806c9.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/React-Portfolio/static/js/2.d66806c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecbf00f97fa1de4ccd8d",
+    "url": "/React-Portfolio/static/js/3.ab15826f.chunk.js"
+  },
+  {
+    "revision": "4947a760b936f36dff7f",
+    "url": "/React-Portfolio/static/js/main.98d2cf09.chunk.js"
+  },
+  {
+    "revision": "257c66147334eeadf3d6",
+    "url": "/React-Portfolio/static/js/runtime-main.9e8a278a.js"
+  }
+]);
