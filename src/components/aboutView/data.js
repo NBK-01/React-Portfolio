f3d03aac5600acbafa3bc8a799cc8414e9ce826d
unknown <nayefkanaan03@gmail.com>
2021-11-19 17:47:22 +0300

    Adding init about info data object

---
+++ src/components/aboutView/data.js
@@ -0,0 +1,12 @@
+export const aboutObjOne = {
+id: 'learner',
+lightBg: false,
+lightText: true,
+lightTextDesc: true,
+topLine: 'LEarningf djfs', 
+headline: 'LEarning never stops',
+description: 'BLah blah blahblahblahb  blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah',
+imgStart: false,
+img: require('../../images/learn.png'),
+alt: 'reading book' 
+}
